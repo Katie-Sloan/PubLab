66a68e6a6846bbdc5923bcc05fd2ca54f4bc4273
Mark <markmeldrum70@gmail.com>
2020-10-21 10:47:20 +0100

    initial commit

---
+++ run_tests.py
@@ -0,0 +1,8 @@
+import unittest
+from tests.pub_test import TestPub # imports class
+
+if __name__ == '__main__':
+    unittest.main()
+
+# Allows our code to attach to the test files
+
---
+++ scr/pub.py
@@ -0,0 +1,6 @@
+class Pub:
+    def __init__(self, import_name, import_till):
+        self.name = import_name
+        self.till = import_till
+
+    
