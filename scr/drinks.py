528b68e70a0a517fa0c8072036f61a0b4ee31e57
Mark <markmeldrum70@gmail.com>
2020-10-21 11:48:58 +0100

    First three MVP complete

--- scr/drinks.py
+++ scr/drinks.py
@@ -1,2 +1,5 @@
-
+class Drink:
+    def __init__(self, import_drink_name, import_drink_price):
+        self.drink_name = import_drink_name
+        self.drink_price = import_drink_price
         
--- tests/customer_test.py
+++ tests/customer_test.py
@@ -6,4 +6,13 @@ class TestCustomer(unittest.TestCase):
     def setUp(self):
         self.customer = Customer("Katie", 50.00, 28)
 
-    
+    def test_customer_has_name(self):
+        self.assertEqual('Katie', self.customer.customer_name)
+
+
+    def test_customer_has_money(self):
+        self.assertEqual(50.00, self.customer.customer_wallet)
+
+
+    def test_customer_age(self):
+        self.assertEqual(28, self.customer.customer_age)
